1988e41c8a2192c66afa68315f2b27e4291ecae6
sixtyeight <christian_sl@gmx.at>
2017-04-02 03:50:38 +0200

    added zumtobel examples

---
+++ src/main/java/at/metalab/artnet/HelloArtnetZumtobelRGB.java
@@ -0,0 +1,75 @@
+package at.metalab.artnet;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import artnet4j.ArtNet;
+import artnet4j.packets.ArtDmxPacket;
+
+public class HelloArtnetZumtobelRGB {
+
+	public static void main(String[] args) throws Exception {
+		new HelloArtnetZumtobelRGB().go(args);
+	}
+
+	public void go(String[] args) throws Exception {
+		ArtNet artnet = new ArtNet();
+
+		artnet.start();
+		
+		byte[] color1 = new byte[] {};
+		// Lampe 1
+		color1 = ArrayUtils.add(color1, (byte) 0);   // r
+		color1 = ArrayUtils.add(color1, (byte) 255); // g
+		color1 = ArrayUtils.add(color1, (byte) 255); // b
+		color1 = ArrayUtils.add(color1, (byte) 0);   // a
+		color1 = ArrayUtils.add(color1, (byte) 0);   // w
+		// Lampe 2
+		color1 = ArrayUtils.add(color1, (byte) 0);   // r
+		color1 = ArrayUtils.add(color1, (byte) 255); // g
+		color1 = ArrayUtils.add(color1, (byte) 255); // b
+		color1 = ArrayUtils.add(color1, (byte) 0);   // a
+		color1 = ArrayUtils.add(color1, (byte) 0);   // w
+
+		byte[] color2 = new byte[] {};
+		// Lampe 1
+		color2 = ArrayUtils.add(color2, (byte) 0); // r
+		color2 = ArrayUtils.add(color2, (byte) 0); // g
+		color2 = ArrayUtils.add(color2, (byte) 0); // b
+		color2 = ArrayUtils.add(color2, (byte) 0); // a
+		color2 = ArrayUtils.add(color2, (byte) 0); // w
+		// Lampe 2
+		color2 = ArrayUtils.add(color2, (byte) 0); // r
+		color2 = ArrayUtils.add(color2, (byte) 0); // g
+		color2 = ArrayUtils.add(color2, (byte) 0); // b
+		color2 = ArrayUtils.add(color2, (byte) 0); // a
+		color2 = ArrayUtils.add(color2, (byte) 0); // w
+
+		int universe = 1; // Zumtobel
+		String ip = "10.20.255.255"; // n.b. broadcast
+	
+		ArtDmxPacket packetColor1= new ArtDmxPacket();
+		packetColor1.setUniverse(0, universe);
+		packetColor1.setSequenceID(0);
+		packetColor1.setDMX(color1, color1.length);
+
+		ArtDmxPacket packetColor2 = new ArtDmxPacket();
+		packetColor2.setUniverse(0, universe);
+		packetColor2.setSequenceID(0);
+		packetColor2.setDMX(color2, color2.length);
+		
+		for (;;) {
+			Thread.sleep(1000);
+			System.out.println("color1");
+			for(int i = 0; i < 10; i++) { // send multiple times due to possible packet loss
+				artnet.unicastPacket(packetColor1, ip);
+			}
+			
+			Thread.sleep(1000);
+			System.out.println("color2");
+			for(int i = 0; i < 10; i++) { // send multiple times due to possible packet loss
+				artnet.unicastPacket(packetColor2, ip);
+			}
+		}
+	}
+
+}
